4e4a29106314e9c051216a06049568c5c1e43b51
antoine burgaudeau <antoine.burgaudeau@gmail.com>
2020-11-18 23:38:56 +0100

    port 4005

--- WorkShop_QRCar/qrcar_micro_car_identification/config/dev.exs
+++ WorkShop_QRCar/qrcar_micro_car_identification/config/dev.exs
@@ -16,7 +16,7 @@ config :qrcar_micro_car_identification, QrcarMicroCarIdentification.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :qrcar_micro_car_identification, QrcarMicroCarIdentificationWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 4005],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
